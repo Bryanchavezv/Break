15a0270e7e06df457086658058cdc0c825aadf74
bryanchavezv <bryanchavezale@gmail.com>
2024-05-24 21:47:45 -0400

    Ediciones y se añadieron avatares Co-authored-by: EmilFuentes <EmilFuentes@users.noreply.github.com>

--- assets/js/validacion.js
+++ assets/js/validacion.js
@@ -1,32 +1,69 @@
+/**
+ * Validar formulario de registro
+ */
 function validateForm() {
-    const email = document.getElementById('formEmail').value;
-    const password = document.getElementById('formContraseña').value;
-    const rol = document.getElementById('formRol').value;
-    
+  const email = document.getElementById('formEmail').value;
+  const password = document.getElementById('formContraseña').value;
+  const rol = document.getElementById('formRol').value;
   
-    if (!validateEmail(email)) {
-      alert('Por favor, ingresa un correo electrónico válido.');
-      return false; // Debes retornar false para evitar que se envíe el formulario si hay errores
-    }
-  
-    if (!validatePassword(password)) {
-      alert('La contraseña debe tener al menos 8 caracteres, incluyendo letras mayúsculas, minúsculas, números y caracteres especiales.');
-      return false; // Debes retornar false para evitar que se envíe el formulario si hay errores
-    }
-  
-   
-    return true; // Retorna true para permitir el envío del formulario si todas las validaciones son exitosas
+
+  if (!validateEmail(email)) {
+    alert('Por favor, ingresa un correo electrónico válido.');
+    return false; 
   }
-  
+
+  if (!validatePassword(password)) {
+    alert('La contraseña debe tener al menos 8 caracteres, incluyendo letras mayúsculas, minúsculas, números y caracteres especiales.');
+    return false; 
+  }
+
+  // Redirige a inicio.html solo después de que se completen las validaciones
+  window.location.href = "inicio.html";
+  return false; 
+}
+
+
   function validateEmail(email) {
-    // Expresión regular para validar el formato de correo electrónico
+   
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   }
   
   function validatePassword(password) {
-    // Expresión regular para validar la contraseña
+    
     const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
     return passwordRegex.test(password);
   }
 
+/**
+ * Validar formulario de login
+ */
+function validateLoginForm() {
+  const email = document.getElementById('formEmaillogin').value; 
+  const password = document.getElementById('formContraseñalogin').value; 
+
+  if (!validateEmail(email)) {
+    alert('Por favor, ingresa un correo electrónico válido.');
+    return false; 
+  }
+
+  if (!validatePassword(password)) {
+    alert('La contraseña debe tener al menos 8 caracteres, incluyendo letras mayúsculas, minúsculas, números y caracteres especiales.');
+    return false; 
+  }
+
+  // Si las validaciones son exitosas, permitir el envío del formulario y redirigir a inicio.html
+  window.location.href = "inicio.html";
+  return false; 
+}
+
+
+function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+  return passwordRegex.test(password);
+}
