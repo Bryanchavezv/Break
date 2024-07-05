2c8f8090a51292cab3dd1eba9008b50d06ee4fba
BryanChavez <brya.chavez@duocuc.cl>
2024-07-05 19:54:21 -0400

    Configuracion URLS corregir

--- break/redsocial/urls.py
+++ break/redsocial/urls.py
@@ -1,9 +1,11 @@
-
-from django.contrib import admin
-from django.urls import path, include #(include)redirecciona al urls.py de alumnos
+from django.urls import path
+from . import views  # Importa las vistas
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include ('redsocial.urls')),#redirecciona al urls.py de alumnos(cuando no haya nada '' muestra 'alumnos.urls')
+    path('', views.index, name='index'),  # Ruta para el index
+    path('contraseña/', views.contraseña, name='contraseña'),  # Ruta para cambiar contraseña
+    path('editar_perfil/', views.editar_perfil, name='editar_perfil'),  # Ruta para cambiar editar_perfil
+    path('inicio/', views.inicio, name='inicio'),  # Ruta para cambiar inicio
+    path('login/', views.login, name='login'),  # Ruta para cambiar login
+    path('registrar/', views.registrar, name='registrar'),  # Ruta para registrar
 ]
-
