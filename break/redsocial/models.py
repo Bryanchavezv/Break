c677232718bcb82ce66639c9ccc74bd8ff499c88
BryanChavez <brya.chavez@duocuc.cl>
2024-07-05 22:10:20 -0400

    Forms, Modelos Tabla

--- break/redsocial/models.py
+++ break/redsocial/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.conf import settings
+
+class Rol(models.Model):
+    id_rol = models.AutoField(primary_key=True, db_column='idrol')
+    rol = models.CharField(max_length=20, blank=False, null=False)
+
+    def __str__(self):
+        return self.rol
+
+class Genero(models.Model):
+    id_genero = models.AutoField(primary_key=True, db_column='idgenero')
+    genero = models.CharField(max_length=20, blank=False, null=False)
+
+    def __str__(self):
+        return self.genero
+
+class Usuario(models.Model):
+    rut = models.CharField(primary_key=True, max_length=10)
+    nombre = models.CharField(max_length=20)
+    apellido_paterno = models.CharField(max_length=20)
+    apellido_materno = models.CharField(max_length=20)
+    fecha_nacimiento = models.DateField(blank=False, null=False)
+    id_rol = models.ForeignKey(Rol, on_delete=models.CASCADE, db_column='idrol')
+    id_genero = models.ForeignKey(Genero, on_delete=models.CASCADE, db_column='idgenero')
+    telefono = models.CharField(max_length=45)
+    email = models.EmailField(unique=True, max_length=100, blank=True, null=True)
+    direccion = models.CharField(max_length=50, blank=True, null=True)
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido_paterno}"
 
 
+class MensajeForo(models.Model):
+    id_mensaje = models.AutoField(primary_key=True)
+    autor = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    contenido = models.TextField()
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Mensaje por {self.autor} el {self.fecha_publicacion}"
+
+class Libro(models.Model):
+    isbn = models.CharField(primary_key=True, max_length=13)
+    titulo = models.CharField(max_length=100)
+    autor = models.CharField(max_length=50)
+    editorial = models.CharField(max_length=50)
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.titulo
+
+class CompraLibro(models.Model):
+    id_compra = models.AutoField(primary_key=True)
+    libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField()
+    fecha_compra = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Compra de {self.cantidad} unidades de {self.libro.titulo} el {self.fecha_compra}"
--- break/redsocial/views.py
+++ break/redsocial/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import RegistroUsuarioForm,LoginForm
+
+
 
 def index(request):
 
@@ -20,3 +24,42 @@ def registrar(request):
 
 def editar_perfil(request):
     return render(request, 'redsocial/editar_perfil.html')
+
+
+
+def registrar_usuario(request):
+    if request.method == 'POST':
+        form = RegistroUsuarioForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            # Redirigir al usuario a la página deseada después del registro
+            return redirect('redsocial/inicio.html')  # Cambia 'pagina_principal' por la URL a la que deseas redirigir
+    else:
+        form = RegistroUsuarioForm()
+    return render(request, 'redsocial/registro.html', {'form': form})
+
+
+
+def iniciar_sesion(request):
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                # Redirigir al usuario a la página deseada después del inicio de sesión
+                return redirect('redsocial/inicio.html')  # Cambia 'pagina_principal' por la URL a la que deseas redirigir
+            else:
+                # Manejar el caso cuando las credenciales no son válidas
+                # Por ejemplo, mostrar un mensaje de error
+                form.add_error(None, 'Usuario o contraseña incorrectos')
+    else:
+        form = LoginForm()
+    
+    return render(request, 'redsocial/login.html', {'form': form})
