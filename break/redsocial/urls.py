560af7ee9ace98e20208f36d6e4f1b246dedea1b
BryanChavez <brya.chavez@duocuc.cl>
2024-07-05 21:20:04 -0400

    Correciones de codigo

--- break/redsocial/urls.py
+++ break/redsocial/urls.py
@@ -4,7 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), 
-    path('', views.login, name='login'), 
+    path('login', views.login, name='login'), 
+    path('registrar', views.registrar, name='registrar'), 
+    path('inicio', views.inicio, name='inicio'), 
+    path('editar_perfil', views.editar_perfil, name='editar_perfil'), 
+    path('contraseña', views.contraseña, name='contraseña'), 
+   
+   
+
 
 ]
 
--- break/redsocial/views.py
+++ break/redsocial/views.py
@@ -6,3 +6,17 @@ def index(request):
 
 def login(request):
     return render(request, 'redsocial/login.html')
+
+
+def contraseña(request):
+    return render(request, 'redsocial/contraseña.html')
+
+def inicio(request):
+    return render(request, 'redsocial/inicio.html')
+
+
+def registrar(request):
+    return render(request, 'redsocial/registrar.html')
+
+def editar_perfil(request):
+    return render(request, 'redsocial/editar_perfil.html')
